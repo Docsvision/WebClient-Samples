d02362d0806892a3b24613c71e81e05ecd2d44c0
Elkhov Denis <Elkhov.D@docsvision.com>
2023-02-22 17:23:40 +0300

    BA-738 Пример расширения сервиса согласования

---
+++ Others/CustomStageService/CustomStageService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using DocsVision.ApprovalDesigner.ObjectModel;
+using DocsVision.ApprovalDesigner.ObjectModel.Services;
+using DocsVision.BackOffice.ObjectModel;
+using DocsVision.BackOffice.ObjectModel.Services;
+
+namespace CustomStageService
+{
+	/// <summary>
+	/// Пример переопределения сервиса этапа
+	/// </summary>
+	public class CustomStageService : ApprovalStageService
+	{
+		/// <summary>
+		/// Переопределяем вычисления согласующих для этапа
+		/// </summary>
+		/// <param name="approvalStage">Текущий этап</param>
+		/// <param name="document">Документ</param>
+		/// <returns></returns>
+		public override IEnumerable<ApprovalStageApprover> GetStageApprovers(ApprovalStage approvalStage, Document document)
+		{
+			List<ApprovalStageApprover> approvers = new List<ApprovalStageApprover>();
+
+			IStaffService staffService = Context.GetService<IStaffService>();
+			StaffEmployee employee = staffService.FindEmpoyeeByAccountName(@"Test\TestEmployee");
+
+			// CreateApprover - штатный метод ApprovalStageService создающий запись согласующего из сотрудника
+			approvers.Add(CreateApprover(employee));
+
+			return approvers;
+		}
+
+		/// <summary>
+		/// Дополняем создание задания согласования
+		/// </summary>
+		/// <param name="approvalStage">Текущий этап</param>
+		/// <param name="approvalPath">Маршрут</param>
+		/// <param name="reconcileCard">Согласование</param>
+		/// <param name="task">Созданное задание</param>
+		protected override void OnTaskCreated(ApprovalStage approvalStage, ApprovalPath approvalPath, Reconcile reconcileCard, Task task)
+		{
+			// дополним содержание задания
+			task.MainInfo.Content += Environment.NewLine + "Тест расширения";
+		}
+	}
+}
---
+++ Others/CustomStageService/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DocsVision Approval Designer Get Approvers Sample")]
+[assembly: AssemblyDescription("DocsVision Approval Designer Get Approvers Sample")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("326EA9D5-E23A-4DDA-91F9-A14608434D27")]
