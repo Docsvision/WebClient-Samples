155f5d03255f7034e3844dfe09c8af46023611d5
Kolyaev Evgeny <kolyaev.e@docsvision.com>
2024-02-27 11:41:28 +0300

    Added file conversion sample

---
+++ ServerExtensions/ConversionSample/ConversionSampleWebExtension/src/EventHandlers/AttachPdfa.tsx
@@ -0,0 +1,79 @@
+﻿import { Layout } from "@docsvision/webclient/System/Layout";
+import { layoutManager } from "@docsvision/webclient/System/LayoutManager";
+import { $ConversionFileController } from "../Services/ConversionSample";
+import { FileListControl } from "@docsvision/webclient/BackOffice/FileListControl";
+import { ICancelableEventArgs } from "@docsvision/webclient/System/ICancelableEventArgs";
+import { IAgreementEventArgs } from "@docsvision/webclient/Approval/IAgreementEventArgs";
+import { StackingModal } from "@docsvision/webclient/Helpers/StackingModal";
+import React from "react";
+import { ModalDialog, ModalDialogContent } from "@docsvision/web/components/modals/modal-dialog";
+import { ModalDialogTopBorder } from "@docsvision/webclient/Helpers/ModalDialog/ModalDialogTopBorder";
+import { ModalHost } from "@docsvision/webclient/Helpers/ModalHost";
+import { resources } from "@docsvision/webclient/System/Resources";
+import { AgreementManagement } from "@docsvision/webclient/Approval/AgreementManagement";
+import { MessageBox } from "@docsvision/webclient/Helpers/MessageBox/MessageBox";
+
+export async function attachPdfa(sender: Layout, args: ICancelableEventArgs<IAgreementEventArgs>) {
+    args.wait();
+
+    const conversionService = sender.getService($ConversionFileController);
+    const fileList = layoutManager.cardLayout.controls.get<FileListControl>("fileList");
+    const fileId = fileList.params.files[0].data.currentVersion.id;
+    try
+    {
+        await conversionService.сanConvert(fileId);
+
+        const modalHost = new ModalHost("conversion-dialog", () => renderConversionInfo());
+        try {
+            const agreementManagement = layoutManager.cardLayout.controls.get<AgreementManagement>("agreementManagementButton");
+            agreementManagement.hideSidebar();
+            modalHost.mount();
+            await conversionService.attachPdfa(layoutManager.cardLayout.cardInfo.id, fileId);
+            modalHost.unmount();
+            await fileList.reloadFromServer();
+            args.accept();
+        } catch (err) {
+            args.cancel();
+            modalHost.unmount();
+            throw err;
+        }
+    } catch (err) {
+        MessageBox.ShowError(err);
+        throw err;
+    }
+}
+
+export async function attachPdfaButton(sender: Layout) {
+    const conversionService = sender.getService($ConversionFileController);
+    const fileList = layoutManager.cardLayout.controls.get<FileListControl>("fileList");
+    const fileId = fileList.params.files[0].data.currentVersion.id;
+
+    try {
+        await conversionService.сanConvert(fileId);
+        const modalHost = new ModalHost("conversion-dialog", () => renderConversionInfo());
+        try {
+            const agreementManagement = layoutManager.cardLayout.controls.get<AgreementManagement>("agreementManagementButton");
+            agreementManagement.hideSidebar();
+            modalHost.mount();
+            await conversionService.attachPdfa(layoutManager.cardLayout.cardInfo.id, fileId);
+            modalHost.unmount();
+            await fileList.reloadFromServer();
+        } catch (err) {
+            modalHost.unmount();
+            throw err;
+        }
+    } catch (err) {
+        throw err;
+    }
+}
+
+function renderConversionInfo() {
+    return (
+        <StackingModal visible={true} >
+            <ModalDialog isOpen={true} showCloseButton={false} header={resources.Dialog_Information} showButtonsPanel={false}>
+                    <ModalDialogTopBorder color="#4285F4" /> 
+                    <ModalDialogContent>{resources.ConvertationProcess}</ModalDialogContent>
+            </ModalDialog>
+        </StackingModal>
+    );
+}
---
+++ ServerExtensions/ConversionSample/README.md
@@ -0,0 +1,30 @@
+﻿# ConverionSampleServerExtension
+
+Этот пример серверного расширения демонстрирует пример взаиможействия с сервисом конвертации.
+
+## Настройка среды
+
+**Перечень необходимых инструментов:** 
+* [Visual Studio 2019+](https://www.visualstudio.com)
+* [NodeJS v14.17.0+](https://nodejs.org/en/)
+
+> Для работы расширения требуется наличие в лицензии фичи **Docsvision Сервис конвертации файлов**.
+
+## Сборка
+
+1. Открыть /Samples.sln
+2. Собрать проект ServerExtensions > ConverionSample > ConverionSampleServerExtension
+3. Открыть консоль в папке ServerExtensions > ConverionSample > ConversionSampleWebExtension `npm install`, `npm update` и `npm run build:prod`
+4. Скопировать каталог SamplesOutput\Site\Content\Modules\ConversionSampleWebExtension в каталог "Путь к установленному Web-клиент\Site\Content\Modules"
+5. Скопировать каталог SamplesOutput\Site\Extensions\ConverionSampleServerExtension в каталог "Путь к установленному Web-клиент\Site\Extensions"
+6. Перезапустить IIS на сервере Docsvision 5.
+7. Перезапустить IIS на сервере Docsvision 5 Web-клиент.
+
+## Проверка примера
+
+> Для работы расширения требуется задать адрес Web-клиента в Справочнике системных настроек и адрес сервера Р7/OnlyOffice в конфигурационном файле Web-клиента.
+
+1. Открыть конструктор веб-разметок.
+2. В разметке просмотра документа на событие "Перед стартом согласования" элемента управления agreementManagement назначить обработчик attachPdfa.
+3. Создать карточку документа, приложить к ней .docx-файл и отправить на согласование.
+4. К документу будет добавлен первый файл из списка, сконвертированный в PDF/A.
